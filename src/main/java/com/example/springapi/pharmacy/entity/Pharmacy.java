108966308381bfec2e80692a415c218246c66e23
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-02-24 17:26:05 +0900

    jpa Dirty Checking test jpa의 dirty checking에 대한 이해를 위해 간단한 테스트 작성 transaction 안에서 update와 non transaction 안에서 update를 통해 test

--- src/main/java/com/example/springapi/pharmacy/entity/Pharmacy.java
+++ src/main/java/com/example/springapi/pharmacy/entity/Pharmacy.java
@@ -25,4 +25,9 @@ public class Pharmacy {
     private String pharmacyAddress;
     private double latitude;
     private double longitude;
+
+    public void changePharmacyAddress(String address) {
+        this.pharmacyAddress = address;
+    }
+
 }
