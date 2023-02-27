e91ddcb72640408ff78d9133cef4497d6c34684d
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-02-27 22:01:53 +0900

    카테고리 검색기능을 uri builder 서비스에 추가 및 테스트 검증 kakao에서 제공하는 카테고리를 통한 장소 검색은 다음과 같은 파라미터를 요구한다. - category_group_code - x (longitude) - y (latitude) - radius 선택적으로 sort 기능도 넣을 수 있음 - accuracy (default value 이다) - distance

--- src/test/groovy/com/example/springapi/api/service/KakaoUriBuilderServiceTest.groovy
+++ src/test/groovy/com/example/springapi/api/service/KakaoUriBuilderServiceTest.groovy
@@ -15,7 +15,7 @@ class KakaoUriBuilderServiceTest extends Specification {
 
     def "buildUriByAddressSearch - 한글 파라미터의 경우 정상적으로 인코딩"() {
         given:
-        String address = "서울 성북구"
+        String address = "서울시 강동구"
         def charset = StandardCharsets.UTF_8
 
         when:
@@ -23,6 +23,22 @@ class KakaoUriBuilderServiceTest extends Specification {
         def decodedResult = URLDecoder.decode(uri.toString(), charset)
 
         then:
-        decodedResult == "https://dapi.kakao.com/v2/local/search/address.json?query=서울 성북구"
+        decodedResult == "https://dapi.kakao.com/v2/local/search/address.json?query=서울시 강동구"
+    }
+
+    def "buildUriByCategorySearch - 요구하는 파라미터를 모두 제공시 정상적으로 인코딩"() {
+        given:
+        String categoryGroupCode = "PM9"
+        double latitude = 37
+        double longitude = 127
+        double radius = 1
+        def charset = StandardCharsets.UTF_8
+
+        when:
+        def uri = kakaoUriBuilderService.buildUriByCategorySearch(latitude, longitude, radius, categoryGroupCode)
+        def decodedResult = URLDecoder.decode(uri.toString(), charset)
+
+        then:
+        decodedResult ==  "https://dapi.kakao.com/v2/local/search/category.json?category_group_code=PM9&x=127.0&y=37.0&radius=1000.0&sort=distance"
     }
 }
