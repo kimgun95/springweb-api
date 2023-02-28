95bad54163f040c786209d91313e0956f5270633
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-03-01 01:10:29 +0900

    redis를 이용한 자료 구조 구현 및 '약국 검색시' redis에서 탐색하기 구현 - 데이터 초기 셋팅을 위한 임시 컨트롤러 구현 : database에 있는 data를 redis에 저장한다 - RedisTemplate을 통해 HashOperation을 이용하여 pharmacyDto를 조회,저장 구현 - PharmacySearchService에 redis에서 약국 탐색하는 로직을 추가

--- src/main/java/com/example/springapi/pharmacy/service/PharmacySearchService.java
+++ src/main/java/com/example/springapi/pharmacy/service/PharmacySearchService.java
@@ -1,5 +1,6 @@
 package com.example.springapi.pharmacy.service;
 
+import com.example.springapi.pharmacy.cache.PharmacyRedisTemplateService;
 import com.example.springapi.pharmacy.dto.PharmacyDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +15,16 @@ import java.util.stream.Collectors;
 public class PharmacySearchService {
 
     private final PharmacyRepositoryService pharmacyRepositoryService;
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
 
     public List<PharmacyDto> searchPharmacyDtoList() {
 
         // redis
+        List<PharmacyDto> pharmacyDtoList = pharmacyRedisTemplateService.findAll();
+        if(!pharmacyDtoList.isEmpty()) {
+            log.info("redis findAll success!");
+            return pharmacyDtoList;
+        }
 
         // db
         return pharmacyRepositoryService.findAll()
